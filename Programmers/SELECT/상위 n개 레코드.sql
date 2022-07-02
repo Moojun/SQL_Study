e0e4d1702ed99881bbafbd98c0c5932d8d2c5158
Moojun <80478750+Moojun@users.noreply.github.com>
2022-07-02 16:26:50 +0900

    Update 상위 n개 레코드.sql

--- Programmers/SELECT/상위 n개 레코드.sql
+++ Programmers/SELECT/상위 n개 레코드.sql
@@ -1,4 +1,18 @@
 /* use Subquery */
 SELECT NAME 
 FROM ANIMAL_INS
-WHERE DATETIME = (SELECT MIN(DATETIME) FROM ANIMAL_INS);
+WHERE DATETIME = (SELECT MIN(DATETIME) FROM ANIMAL_INS;
+                  
+                  
+/* use Order by, Limit */
+SELECT NAME 
+FROM ANIMAL_INS
+ORDER BY DATETIME
+LIMIT 1;                  
+                  
+/*
+stackoverflow: max/min vs order by + limit
+https://stackoverflow.com/questions/426731/min-max-vs-order-by-and-limit                  
+                  
+*/                  
+             
